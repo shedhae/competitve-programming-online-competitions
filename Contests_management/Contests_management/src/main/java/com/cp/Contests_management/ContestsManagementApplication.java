cbe34b66a008eef80b0cc96d28ec726484e3e373
MohamedBouafif <mohamedbouafif2020@gmail.com>
2025-03-27 20:48:56 +0100

    intilizing springboot project and connecting to DB using postgreSQL

---
+++ Contests_management/Contests_management/src/main/java/com/cp/Contests_management/ContestsManagementApplication.java
@@ -0,0 +1,14 @@
+package com.cp.Contests_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestsManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ContestsManagementApplication.class, args);
+	}
+
+}
